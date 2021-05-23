3b63ab6868e42617e7c4c58374239552238b5176
Abhinav499 <raagaagupta@gmail.com>
2021-05-23 16:31:45 +0530

    hi

---
+++ sketch.js
@@ -0,0 +1,56 @@
+var hr,min,sec;
+
+function setup() {
+  createCanvas(800,400);
+  createSprite(400, 200, 50, 50);
+angleMode(DEGREES);
+
+}
+
+function draw() {
+  background(0); 
+hr=hour();
+min=minute();
+sec=second();
+translate (400,200);
+rotate(-90);
+secAngle= map(sec,0,60,0,360);
+minAngle=map(min,0,60,0,360);
+hrAngle=map(hr%12,0,12,0,360);
+
+push();
+rotate(secAngle);
+stroke(255,0,0);
+strokeWeight(7);
+line(0,0,100,0);
+pop ();
+
+push ();
+rotate(minAngle);
+stroke(0,0,255);
+strokeWeight(7);
+line(0,0,70,0);
+pop ();
+
+push ();
+rotate(hrAngle);
+stroke(0,255,0);
+strokeWeight(7);
+line(0,0,50,0);
+pop ();
+
+
+noFill();
+strokeWeight(10);
+stroke(255,0,0);
+arc (0,0,300,300,0,secAngle);
+
+
+stroke(0,0,255);
+arc(0,0,280,280,0,minAngle);
+
+stroke(0,255,0);
+arc(0,0,260,260,0,hrAngle);
+
+  drawSprites();
+}
